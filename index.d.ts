361935c8c85494023b51e20f139af78ad68b6926
Frank Matranga <thefrankmatranga@gmail.com>
2022-01-23 00:35:24 -0500

    Export util types

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,4 @@ export * from "./interfaces/donations";
 export * from "./interfaces/events";
 export * from "./interfaces/tickets";
 export * from "./interfaces/users";
+export * from "./interfaces/utils";
