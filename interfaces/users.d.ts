58ef2d41086046eb6351262a6bc4eaaed497e769
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-05 17:32:11 -0500

    Change to declaration files

--- interfaces/users.d.ts
+++ interfaces/users.d.ts
@@ -1,5 +1,4 @@
 import { Contact } from "./contact";
-import { Address } from "./utils";
 
 /** Represents a Firestore user document from the `users` collection. */
 export interface UserDocument extends Contact {
