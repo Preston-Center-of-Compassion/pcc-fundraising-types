5ffd2f18c46f282e1b9a381311e102fbad7560b3
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-13 20:03:06 -0400

    Switch to generic eventData

--- interfaces/contact.d.ts
+++ interfaces/contact.d.ts
@@ -2,15 +2,15 @@ import { Address } from "./utils";
 
 /** Represents a person's contact information. */
 export interface Contact {
-  firstName?: string;
-  lastName?: string;
-  homeAddress?: Address;
-  email?: string;
+  firstName: string;
+  lastName: string;
+  homeAddress: Address;
+  email: string;
   /**
    * Contact's phone number in E.164 format.
    * Example: +13478519465
    *
    * See for details: https://www.twilio.com/docs/glossary/what-e164
    **/
-  phoneNumber?: string;
+  phoneNumber: string;
 }
--- interfaces/events.d.ts
+++ interfaces/events.d.ts
@@ -5,46 +5,53 @@ export type EventAlert = {
   /** Short title of alert. */
   title?: string;
   /** Optional long body text of alert. */
-  body?: string;
+  body: string;
   /** Exact timestamp for when the alert was posted. */
-  postedAt?: Timestamp;
+  postedAt: Timestamp;
 };
 
 /** Represents a particular rate for a number of tickets. */
 export type TicketPrice = {
-  ticketCount?: number;
-  priceInCents?: number;
+  ticketName?: string;
+  ticketCount: number;
+  priceInCents: number;
 };
 
 /** Represents a Firestore document in the `/events` collection. */
-export interface EventDocument {
+export interface EventDocument<T> {
   /** Public-facing name of event. */
-  name?: string;
+  name: string;
   /** Optional subtitle of event. */
   lead?: string;
   /** Optional alerts for event. */
-  alerts?: EventAlert[];
+  alerts: EventAlert[];
   /** Type of event. */
-  type?: "august" | "50-50" | "bonus" | "chinese-auction";
+  type: "august" | "50-50" | "bonus" | "chinese-auction";
   /** Exact timestamp for when event opens to public. */
   openAt?: Timestamp;
   /** Exact timestamp for when event closes to public. */
   closeAt?: Timestamp;
-  /**
-   * List of tickets prices and discounts.
-   *
-   * Examples:
-   * - [{ 1, 1000 }] means every ticket is $10
-   * - [{10, 8000}, {5, 4000}, {1, 1000}] means 10 tickets for $80, 5 for $40, etc.
-   **/
-  ticketPricing?: TicketPrice[];
+  // /**
+  //  * List of tickets prices and discounts.
+  //  *
+  //  * Examples:
+  //  * - [{ 1, 1000 }] means every ticket is $10
+  //  * - [{10, 8000}, {5, 4000}, {1, 1000}] means 10 tickets for $80, 5 for $40, etc.
+  //  **/
+  // ticketPricing?: TicketPrice[];
+  eventData: T;
 }
 
-export interface FiftyFiftyEventDocument extends EventDocument {}
-
-export interface ChineseAuctionEventDocument extends EventDocument {}
-
-export interface BonusEventDocument extends EventDocument {
+export type BonusEventDocument = {
   ticketMin?: number;
   ticketMax?: number;
-}
+};
+
+type Prize = { title: string; subtitle?: string };
+
+export type AugustEventData = {
+  seatPriceInCents: number;
+  ticketPriceInCents: number;
+  prizes: Prize[];
+  drawingDate: Timestamp;
+};
