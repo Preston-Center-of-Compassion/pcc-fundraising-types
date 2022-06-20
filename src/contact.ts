cde81b76f92493ab5f366b3965cf7ac8471ae1a8
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-20 14:46:11 -0400

    Create zod Contact schema

--- src/contact.ts
+++ src/contact.ts
@@ -6,23 +6,11 @@ export const Contact = z.object({
   firstName: z.string().min(1).max(30).trim(),
   lastName: z.string().min(1).max(30).trim(),
   email: z.string().email(),
-  phoneNumber: z.string().min(1).max(20).trim(),
+  phoneNumber: z
+    .string()
+    .regex(/^\+[1-9]\d{1,14}$/) // E.164 format: https://www.twilio.com/docs/glossary/what-e164
+    .trim(),
   address: StripeAddress,
 });
 
 export type Contact = z.infer<typeof Contact>;
-
-// /** Represents a person's contact information. */
-// export interface Contact {
-//   firstName: string;
-//   lastName: string;
-//   homeAddress: Address;
-//   email: string;
-//   /**
-//    * Contact's phone number in E.164 format.
-//    * Example: +13478519465
-//    *
-//    * See for details: https://www.twilio.com/docs/glossary/what-e164
-//    **/
-//   phoneNumber: string;
-// }
