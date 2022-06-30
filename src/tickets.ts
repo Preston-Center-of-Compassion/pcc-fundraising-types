a51d4e86d4e1eca2ac83cf9a3ad795f9657bf2a1
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-30 12:37:16 -0400

    Add ticket type

--- src/tickets.ts
+++ src/tickets.ts
@@ -4,6 +4,7 @@ import { Owned, Purchaseable } from "./utils";
 export const EventTicket = z
   .object({
     selectedAt: z.date(),
+    type: z.string().optional(), // raffle, dinner-reservation, etc.
     ticketIdentifier: z.string().optional(),
     label: z.string().optional(),
     description: z.string().optional(),
